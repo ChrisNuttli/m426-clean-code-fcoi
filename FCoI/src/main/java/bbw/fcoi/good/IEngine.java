8bf07b0ed466b95b5c234fc16058f61b4a512d12
ChrisNuttli <christian.nuttli@lernende.bbw.ch>
2025-02-27 15:17:56 +0100

    Added comment that defines the used Design Pattern

--- FCoI/src/main/java/bbw/fcoi/good/IEngine.java
+++ FCoI/src/main/java/bbw/fcoi/good/IEngine.java
@@ -3,6 +3,7 @@ package bbw.fcoi.good;
 /**
  * In order to add an Engine, regardless of its type, it must be abstracted as an interface.
  * That way, we can add new types of engines without having to change existing code.
+ * This follows the Proxy Pattern.
  */
 public interface IEngine {
 	void start();
